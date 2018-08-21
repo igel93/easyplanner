8fc69b5a73542f6332ad81ff48e005a7d6042449
Anne_Igel <anne@anneigel.com>
2018-08-21 14:58:16 +1000

    create group event view

--- easy-planner/src/app/app.module.ts
+++ easy-planner/src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { LogInComponent } from './log-in/log-in.component';
+import { CreateGroupEventComponent } from './create-group-event/create-group-event.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LogInComponent
+    LogInComponent,
+    CreateGroupEventComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ easy-planner/src/app/create-group-event/create-group-event.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+
+@Component({
+  selector: 'app-create-group-event',
+  templateUrl: './create-group-event.component.html',
+  styleUrls: ['./create-group-event.component.css']
+})
+export class CreateGroupEventComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
