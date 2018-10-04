aae7be194e1a6103c1fb0f5a2b2645acb53f1d45
fengchaohnsq <41362868+fengchaohnsq@users.noreply.github.com>
2018-10-04 19:24:30 +1000

    I changed the server port and password for connecting remote server

--- server/routes/mysqlConnection.js
+++ server/routes/mysqlConnection.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql');
 // Connect mysql to our database
 var connection = mysql.createConnection({
-  host: 'localhost',
+  host: 'easyplannerdb.cxoi8rzemvxy.us-east-2.rds.amazonaws.com',
   user: 'root',
-  password: 'root',
+  password: '12345678',
   port: 3306,
   database: 'easyplannerdb'
 });
